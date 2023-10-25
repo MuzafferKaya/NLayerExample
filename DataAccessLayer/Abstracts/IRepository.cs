e1a1e1f6d73d4484a82471f0a573b563c3770650
MuzafferKaya <kmuzaffer718@gmail.com>
2023-10-26 00:04:46 +0300

    Add project files.

---
+++ DataAccessLayer/Abstracts/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstracts
+{
+    internal interface IRepository<T> where T : class
+    {
+        Task AddAsync(T entity);
+        Task<List<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int id);
+        Task<T> Update(T entity);
+        Task Delete(T entity);
+    }
+}
---
+++ DataAccessLayer/Repositories/Repository.cs
@@ -0,0 +1,49 @@
+﻿using DataAccessLayer.Abstracts;
+using DataAccessLayer.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly AppDbContext _appDbContext;
+        public Repository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await _appDbContext.Set<T>().AddAsync(entity);
+            await _appDbContext.SaveChangesAsync();
+        }
+
+        public async Task Delete(T entity)
+        {
+            _appDbContext.Set<T>().Remove(entity);
+            await _appDbContext.SaveChangesAsync();
+        }
+
+        public async Task<List<T>> GetAllAsync()
+        {
+            return await _appDbContext.Set<T>().ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _appDbContext.Set<T>().FindAsync(id);
+        }
+
+        public async Task<T> Update(T entity)
+        {
+            _appDbContext.Set<T>().Update(entity);
+            await _appDbContext.SaveChangesAsync();
+            return entity;
+        }
+    }
+}
